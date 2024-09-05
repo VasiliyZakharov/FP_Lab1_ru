26cb5299751da13988dc6f707cd9ce63822c313f
VasiliyZakharov <119455484+VasiliyZakharov@users.noreply.github.com>
2024-09-05 20:18:43 +0300

    Update taylor.fsx

--- taylor.fsx
+++ taylor.fsx
@@ -1,25 +1,64 @@
-// Print a table of a given function f, computed by taylor series
-
-// function to compute
-let f = sin
-
+//вариант 8
+let eps = 0.0001
 let a = 0.0
-let b = 1.0
+let b = 2.0
 let n = 10
 
-// Define a function to compute f using naive taylor series method
-let taylor_naive = f
+let PrintTop =
+    printfn "------------------------------------------------------------------"
+    printfn "|  x  |  Builtin  | Smart Taylor | terms1 | Dumb Taylor | terms2 |"
+    printfn "------------------------------------------------------------------"
 
 
-// Define a function to do the same in a more efficient way
-let taylor = f
+let PrintTabl x B (S, t1) (D, t2) =
+    printfn "| %.1f | %.5f  |   %.5f   | %6d |  %.5f   | %6d |" x B S t1 D t2
+    printfn "------------------------------------------------------------------"
 
-let main =
-   for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
-// make sure to improve this table to include the required number of iterations
-// for each of the methods
 
+let f (x: float) =
+    (1.0/(2.0 * x - 5.0))
+
+
+let abs x =
+    if x < 0.0 then 
+        (-1.0) * x
+    else x
+
+
+let SmartTaylor eps x =
+    let form (prev: float) (x: float) =
+        prev * (2.0 * x / 5.0)
+
+    let rec check (acc: float) (m: float) (n: int) =
+        if (abs m) < eps then (acc, n - 1)
+        else
+            let next = form m x
+            check (acc + m) next (n + 1)
+
+    check 0.0 -0.2 1
+
+
+let DumbTaylor (eps: float) (x: float) =
+    let form (n: int) (x: float) =
+        -((2.0**(float n - 1.0))*(x**(float n-1.0)))/(5.0**(float n))
+
+    let rec check (acc: float) (n: int) =
+        let k = form n x
+        if (abs k) < eps then
+            acc + k, n
+        else 
+            check (acc + k) (n + 1)
+
+    check 0.0 1
+
+
+
+
+let main = 
+    PrintTop
+    for i = 0 to n do
+        let x = a+(float i)/(float n)*(b-a)
+        PrintTabl x (f x) (SmartTaylor eps x) (DumbTaylor eps x)
+
+        
 main
-
